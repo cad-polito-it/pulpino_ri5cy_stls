9587ec84bfef1810fdf7404640fc69dd07668ae0
Nick Deligiannis <nd.serv.acc@gmail.com>
2023-05-08 10:23:31 -0400

    Added software test library 4 package

---
+++ stl04/main.c
@@ -0,0 +1,14 @@
+#include "tests.h"
+
+int main()
+{
+	test_alu();
+	test_branches();
+	test_csr();
+	test_if();
+	test_ls();
+	test_mul_dot();
+	test_rf();
+	test_rf2();
+	return 0;
+}
---
+++ stl04/tests.h
@@ -0,0 +1,14 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+extern int test_alu(void);
+extern int test_branches(void);
+extern int test_csr(void);
+extern int test_if(void);
+extern int test_ls(void);
+extern int test_mul_dot(void);
+extern int test_rf(void);
+extern int test_rf2(void);
+
+#endif /*TESTS_H */
+
