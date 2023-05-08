71aea96cfcd5c0d18ea49a75e0fd011771ed714a
Nick Deligiannis <nd.serv.acc@gmail.com>
2023-05-08 10:22:53 -0400

    Added software test library 1 package

---
+++ stl01/main.c
@@ -0,0 +1,11 @@
+#include "tests.h"
+
+int main()
+{
+	test_regs();
+	test_alu();
+	test_mul();
+	test_self();
+	test_jumping();
+ 	return 0;
+}
---
+++ stl01/tests.h
@@ -0,0 +1,11 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+extern int test_alu(void);
+extern int test_jumping(void);
+extern int test_mul(void);
+extern int test_regs(void);
+extern int test_self(void);
+
+#endif /*TESTS_H */
+
